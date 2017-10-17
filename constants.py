1f43b14c94dd42232cf685d513cbd68370f1bf47
ramongonze <ramongonze@gmail.com>
2017-10-17 19:24:43 -0200

    Data read finished.

---
+++ constants.py
@@ -0,0 +1,10 @@
+class Point(object):
+	def __init__(self, x=None, y=None, c=None, d=None):
+		self.x = x
+		self.y = y
+		self.c = c
+		self.d = d
+
+#Parameters
+N = None
+P = None
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import constants as c
 import sys
 
 def main():
 	# Data read
+	# Point is a list with c.Point instances, and it represents all the points in the space.
 	f = open(sys.argv[1], 'r')
-	
+	line = f.readline().split()
+	c.N, c.P = int(line[0]), int(line[1])
+	points = []
 	for line in f:
-
-
+		line = line.split()
+		points = points + [c.Point(int(line[0]), int(line[1]), int(line[2]), int(line[3]))]
 
 main()
