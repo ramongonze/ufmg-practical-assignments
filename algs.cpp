900a026071bab6f65e85934275e544f57fd010f1
ramongonze <ramongonze@gmail.com>
2018-06-26 23:52:03 -0300

    Function to create a maze using DFS created.

--- algs.cpp
+++ algs.cpp
@@ -1,47 +1,141 @@
 #include "algs.hpp"
+#include <stdio.h>
 
-void dijkstra(Graph G, int s, int t){
-	priority_queue<pii, vector<pii>, greater<pii> > pq;
-	pq.push(make_pair(0, s));
- 
-	while(pq.size()){
-		pii u = pq.top(); pq.pop();
-		
-		if(G[u.second].visited)
-			continue;
-		
-		G[u.second].visited = true;
-		G[u.second].dist = u.first;
+void dfs(Graph &G, Graph &G2, stack<int> &S, int u){
+	G[u].visited = true;
 
-		if(u.second == t)
-			return;
- 
-		for(int i = 0; i < G[u.second].adjs.size(); ++i){
-			Edge cn = G[u.second].adjs[i];
-			pq.push(make_pair(G[u.second].dist + cn.cost, cn.sink));
+	// Shuffle the order of neighboors in the adjacent list
+	random_shuffle(G[u].adjs.begin(), G[u].adjs.end());
+
+	for(int i = 0; i < G[u].adjs.size(); ++i){
+		int v = G[u].adjs[i];
+		if(G[v].visited == false){
+			G2[u].adjs.push_back(v);
+			G2[v].adjs.push_back(u);
+			S.push(v);
+			dfs(G,G2,S,v);
 		}
 	}
 }
 
-void aStar(Graph G, int s, int t){
+Graph generateMaze(int n, int m){
+	// Each vertex (x,y) has neighboors: (x,y-1), (x+1,y), (x,y+1), (x-1,y)
+
+	srand(3);
+
+	Graph G; // All vertex starts with 4 walls around it.
+	Graph G2; // Solution. Each wall removed from G is added in G2
+	stack<int> S;
+
+	for(int i = 0; i < n; i++){
+		int c = n*i;
+		for(int j = 0; j < m; j++){
+			int u = c + j; // Vertex index
+			G.push_back(Vertex(j,i));
+			G2.push_back(Vertex(j,i));
+			// Left neighboor
+			if(j-1 >= 0) G[u].adjs.push_back(c + j-1);
+
+			// Up neighboor
+			if(i+1 < n) G[u].adjs.push_back(c+n + j);				
+
+			// Right neighboor
+			if(j+1 < m) G[u].adjs.push_back(c + j+1);				
+			
+			// Down neighboor
+			if(i-1 >= 0) G[u].adjs.push_back(c-n + j);	
+		}
+	}
+
+	// DFS execution
+	S.push(0);
+	while(S.size() > 0){
+ 		int u = S.top(); S.pop();
+ 		dfs(G,G2,S,u);
+	}
+
+	return G2;
+}
+
+void printMaze(Graph &G, int n, int m){
+	/*
+	 * Caption:
+	 * l -> left wall
+	 * u -> up wall
+	 * r -> right wall
+	 * d -> down wall
+	*/
+
+	printf("%d,%d\n", n, m); // Print the size of the maze
+
+	for(int i = 0; i < n*m; i++){
+		int l,u,r,d;
+		l = u = r = d = 1;
+
+		for(int j = 0; j < G[i].adjs.size(); j++){
+			// Find broken walls (generated in DFS)
+			int k = G[i].adjs[j];
+			if(G[k].x == G[i].x-1) l = 0;
+			else if(G[k].y == G[i].y+1) u = 0;
+			else if(G[k].x == G[i].x+1) r = 0;
+			else if(G[k].y == G[i].y-1) d = 0;
+		}
+
+		if(l) printf("%d,%d,%d,%d;", G[i].x, G[i].x, G[i].y, G[i].y+1);
+		if(u) printf("%d,%d,%d,%d;", G[i].x, G[i].x+1, G[i].y+1, G[i].y+1);
+		if(r) printf("%d,%d,%d,%d;", G[i].x+1, G[i].x+1, G[i].y+1, G[i].y);
+		if(d) printf("%d,%d,%d,%d;", G[i].x, G[i].x+1, G[i].y, G[i].y);
+	}
+
+	printf("\n");
+}
+
+void dijkstra(Graph &G, int s, int t){
+	priority_queue<pii, vector<pii>, greater<pii> > pq;
+	pq.push(make_pair(0, s));
+ 
+	while(pq.size()){
+		pii a = pq.top(); pq.pop();
+
+		int u = a.second;
+		
+		if(G[u].visited)
+			continue;
+		
+		G[u].visited = true;
+		G[u].dist = a.first;
+
+		if(u == t)
+			return;
+ 
+		for(int i = 0; i < G[u].adjs.size(); ++i){
+			int v = G[u].adjs[i];
+			pq.push(make_pair(G[u].dist + 1, v));
+		}
+	}
+}
+
+void aStar(Graph &G, int s, int t){
 	priority_queue<pii, vector< pii >, greater< pii > > pq;
 	pq.push(make_pair(0, s));
  
 	while(pq.size()){
-		pii u = pq.top(); pq.pop();
+		pii a = pq.top(); pq.pop();
 		
-		if(G[u.second].visited)
+		int u = a.second;
+
+		if(G[u].visited)
 			continue;
 		
-		G[u.second].visited = true;
-		G[u.second].dist = u.first;
+		G[u].visited = true;
+		G[u].dist = a.first;
 
-		if(u.second == t)
+		if(u == t)
 			return;
  
-		for(int i = 0; i < G[u.second].adjs.size(); ++i){
-			Edge cn = G[u.second].adjs[i];
-			pq.push(make_pair(G[u.second].dist + cn.cost + (G[cn.sink]-G[t]), cn.sink));
+		for(int i = 0; i < G[u].adjs.size(); ++i){
+			int v = G[u].adjs[i];
+			pq.push(make_pair(G[u].dist + 1 + (G[v]-G[t]), v));
 		}
 	}
 }
--- algs.hpp
+++ algs.hpp
@@ -1,25 +1,23 @@
 #include <cmath>
 #include <queue>
+#include <stack>
 #include <vector>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
+#define INF 10000000 // Infinite
+
 typedef pair<int,int> pii;
-
-typedef struct Edge{
-	int source, sink;
-	double cost;
-
-	Edge(int _source, int _sink, double _cost){
-		source = _source, sink = _sink, cost = _cost;
-	}
-}Edge;
 
 typedef struct Vertex{
 	int x,y; // Cordinates in an euclidan space
-	double dist; // Used in Dijkstra and A*. Distance between a vertex i and the sink
-	bool visited; // Used in DFS
-	vector<Edge> adjs; // Adjacent list of a vertex
+	double dist; // Used in Dijkstra and A*. Distance between a vertex i and the source
+	bool visited; // Used in DFS, Dijkstra and A*.
+	vector<int> adjs; // Adjacent list of a vertex
+
+	Vertex();
 
 	Vertex(int _x, int _y, int _dist){
 		x = _x, y = _y, dist = _dist;
@@ -27,7 +25,7 @@ typedef struct Vertex{
 
 	Vertex(int _x, int _y){
 		x = _x, y = _y;
-		dist = 0.0;
+		dist = INF;
 		visited = false;
 	}
 
@@ -40,11 +38,34 @@ typedef struct Vertex{
 // Graph representation
 typedef vector<Vertex> Graph;
 
+// Depth-first-search
+void dfs(Graph &G, Graph &G2, stack<int> &S, int u);
+
+/* Generate a maze n x m, where each cell in the grid is a vertex in a graph.
+ * The generated graph has their vertices' indexes sorted according the example below:
+ *
+ * Example of a maze 3x4:
+ *
+ * 	  8  9  10 11
+ * 	  4  5  6  7
+ * 	  0  1  2  3
+ *
+*/
+Graph generateMaze(int n, int m);
+
+/* Generates the coordinates of walls to be printed in a graphic.
+ * Format: wall1;wall2;wall3; ...
+ *
+ * Each wall is a pair of coordinates:
+ * x1,x2,y1,y2;x3,x4,y3,y4; ...
+*/
+void printMaze(Graph G, int n, int m);
+
 // Dijkstra implementation with a priority queue.
-void dijkstra(Graph G, int s, int t);
+void dijkstra(Graph &G, int s, int t);
 
 /*
  * The A* Algorithm implemented here uses the heuristic of euclidean
  * distance between two vertices, where each vertex has coordinates x and y
  */
-void aStar(Graph G, int s, int t);
+void aStar(Graph &G, int s, int t);
