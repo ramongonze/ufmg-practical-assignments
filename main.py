619a6ff46e62723aefe69210e9b808253d4206ec
ramongonze <ramongonze@gmail.com>
2018-10-06 23:39:10 -0300

    Simplex finished. The code passes in all tests. Implemented: Fractions simplify; Print certificates for the original L.P, that is, print only the original variables, not the artificial ones. All comments were revised.

--- main.py
+++ main.py
@@ -5,96 +5,45 @@ import fractions as frac
 """
 	Definitions:
 
-	A = Matrix m x n that represents the constraints
-	b = Vetor of size m that represents the right side of the constraints
-	c = Vector of coeficients
-	x = Vector of variables
+	A = Matrix of constraints. Size m x n.
+	b = Vector of independent terms. Size m.
+	c = Vector of coeficients. Size n.
+	x = Vector of variables. Size n.
 
 	The Linear Programming (LP) will be transformed to the standard form:
 		- Is a maximization problem;
-		- All the constraints are equalities
-		- All the variables are non-negatives
+		- All the constraints are equalities;
+		- All the variables are non-negatives.
 
 	The L.P has this shape:
 
 		 max cx
 		s.t. Ax = b
 			 x >= 0
-
-	GUIDE (from PA-Description):
-	
-	X (a) Read input.
-	
-	X (b) Tranform it in standard form.
-	
-	(c) Run the auxiliar LP to find a base, and verify if it is a feasible problem (this step can be jumped if you find a straightfoward base).
-	
-	X (d) If the problem is feasible, run Simplex and find the optimal solution, or verify that the problem is unlimited.
-	
-	(e) In the end you must write an output file. One extra point will be given for who include certificates in the output file.
 """
-
-################### TESTS ######################
-
-def feasible(A,b,c,y,z):
-	A = np.array(A)
-	b = np.array(b)
-	c = np.array(c)
-	y = y[:(A.shape)[1]]
-	z = z[:(A.shape)[1]]
-
-	print(A)
-	print(b)
-	print(c)
-	print(y)
-	print(z)
-
-	ans = len(np.where(((c-(y@A)) <= 0) == 0)[0]) == 0
-	print(c-(y@A))
-	print("LALALA: " + str((y@A)))
-	print("c - yA <= 0: " + str(ans))
-	ans = (y@b) == (c@z)
-	print("yb = cz: " + str(ans))
-
-def unfeasible(A,b,c,certificate):
-	A = np.array(A)
-	b = np.array(b)
-	c = np.array(c)
-	certificate = certificate[:(A.shape)[1]]
-
-	ans = len(np.where(((certificate@A) >= 0) == 0)[0]) == 0
-	print("yA >= 0: " + str(ans))
-	ans = len(np.where(((certificate@b) < 0) == 0)[0]) == 0
-	print("yb < 0: " + str(ans))
-
-def unlimited(A,b,c,d):
-	A = np.array(A)
-	b = np.array(b)
-	c = np.array(c)
-	d = d[:(A.shape)[1]]
-
-	ans = len(np.where(((A@d) == 0) == 0)[0]) == 0
-	print("Ad = 0: " + str(ans))
-	ans = len(np.where((d >= 0) == 0)[0]) == 0
-	print("d >= 0: " + str(ans))
-	ans = len(np.where(((c@d) > 0) == 0)[0]) == 0
-	print("cd > 0: " + str(ans))
-
-################################################
 
 def readLP(inp_file):
 	"""
-		@Parameter inp_file: directory and name of the input file with the LP
+	@Parameter inp_file: directory/name of the input file with the LP.
+	
+	Return:
+		A : Matrix of constraints
+		b : Vector of independent terms.
+		c : Vector of coeficients.
+		signals: Vector of signals ("<=", ">=" or "==") that represents the 
+				 equalities or inequalities of each constraint.
+		neg_constraints: Binary vector that indicates if variables are free or 
+						 non-negative: 0 for free variable and 1 for 
+						 non-negative variable.
 	"""
 
 	F = open(inp_file, 'r')
 	m = int(F.readline())
 	n = int(F.readline())
 
-	# Read non-negative constraints for variables
+	# Read non-negative constraints of variables.
 	neg_constraints = np.array(F.readline().split(' ')).astype("int")
 
-	#c = np.array(F.readline().split(' ')).astype("double")
 	c = F.readline().split(' ')
 	if c[-1] == '\n':
 		c = c[:-1]
@@ -112,18 +61,32 @@ def readLP(inp_file):
 	signals = [A[i][-2] for i in range(len(A))]
 	A = [A[i][:-2] for i in range(len(A))]
 
-	b = np.array([int(b[i]) for i in range(len(b))])
+	b = np.array([float(b[i]) for i in range(len(b))])
 	for i in range(len(A)):
 		for j in range(len(A[i])):
-			A[i][j] = int(A[i][j])
+			A[i][j] = float(A[i][j])
 		
 	return A,b,c,signals,neg_constraints
 
 def transformFPI(A,c,signals,neg_constraints):
 	"""
-		@Parameter A: Matrix of constraints
-		@Parameter c: Vector of coeficients
-		@Parameter signals: Signals of constraints ("<=",">=" or "==")
+	@Parameter A: Matrix of constraints.
+	@Parameter c: Vector of coeficients.
+	@Parameter signals: Signals of constraints ("<=",">=" or "==")
+	@Parameter neg_constraints: Binary vector that indicates if variables are 
+								free or non-negative: 0 for free variable and 
+								1 for non-negative variable.
+
+	Return:
+		A: Matrix of constraints.
+		c: Vector of coeficients.
+		position: Dictionary used to identify the columns that represent each 
+				  variable in the matrix A.
+				  - Key: variable.
+				  - Value: tuple of columns in the matrix A that represents 
+				  		   the variable.
+				  It is necessary because free variables are replaced by two 
+				  artificial variables, to keep the L.P in the  standard form.
 	"""
 
 	mask = neg_constraints == 0
@@ -172,11 +135,40 @@ def transformFPI(A,c,signals,neg_constraints):
 
 	return A,c,position
 
-def pivot(T,T_cert,x,y):
+def pivot(T,T_cert,x,y,columns):
 	"""
-		@Parameter T: Tableau
-		@Parameter x,y: Pair (x,y) in the tableau T that must be pivoted
+	@Parameter T: Tableau
+	@Parameter T_cert: Auxiliar matrix used to generate certificate of 
+					   optimality.
+	@Parameter x,y: Pair (x,y) in the tableau T that must be pivoted
+	@Parameter columns: Columns that make up a base in the LP
+
+	Return:
+		T: Tableu pivoted in position x,y
+		T_cert: Auxiliar matrix
 	"""
+
+	ind = columns.index(y)
+
+	# If the element T[x,y] == 0, make some elementary operations to make 
+	# possible the pivot.
+	if T[x,y] == 0:
+		for i in range(1,(T.shape)[0]):
+			if i != x and T[i,y] != 0:
+				all_zero = True
+				for j in columns[:ind]:
+					if T[i,j] != 0:
+						all_zero = False
+						break
+
+				if all_zero:
+					T[x,:] = T[x,:] + T[i,:]
+					T_cert[x,:] = T_cert[x,:] + T_cert[i,:]
+					break
+
+	if T[x,y] < 0:
+		T[x,:] *= -1
+
 	v = (1/T[x,y])
 	T[x,:] *= v # Transform the pair x,y in 1
 	T_cert[x,:] *= v
@@ -193,22 +185,34 @@ def pivot(T,T_cert,x,y):
 
 def makeCanonical(T,T_cert,columns):
 	"""
-		@Parameter T: Tableau
-		@Parameter columns: Columns that make up a base in the LP
+	@Parameter T: Tableau
+	@Parameter T_cert: Auxiliar matrix used to generate certificate of 
+					   optimality.
+	@Parameter columns: Columns that make up a base in the LP
+
+	Return:
+		T: Tableu in the canonical form
+		T_cert: Auxiliar matrix
 	"""	
+
 	for i in range(len(columns)):
-		T,T_cert = pivot(T,T_cert,i+1,columns[i])
+		T,T_cert = pivot(T,T_cert,i+1,columns[i],columns)
 
 	return T,T_cert
 
 def minValue(T,col_index,neg):
 	"""
-		@Parameter T: Tableau
-		@Parameter col_index: Index of a negative value in the vector c of 
-							  coeficients
-		@Parameter neg: Boolean vector corresponding to the column 'col_index'
-						in the tableau T. neg[i] will be False if the value 
-						T[i,col_index] <= 0, or True otherwise.
+	@Parameter T: Tableau
+	@Parameter col_index: Index of a negative value in the vector c of 
+						  coeficients.
+	@Parameter neg: Boolean vector corresponding to the column 'col_index' in 
+					the tableau T. neg[i] will be False if the value 
+					T[i,col_index] <= 0, or True otherwise.
+
+	Return:
+		line: Index of line with the minimum value:
+			for each j in the range 1..number_constraints
+				line = min(b[j]/A[j][col_index] : A[j][col_index] > 0)
 	"""
 
 	for v in range(len(neg)):
@@ -226,21 +230,27 @@ def minValue(T,col_index,neg):
 
 def simplex(T,columns):
 	"""
-		@Parameter columns: The vector with indexes of a base of LP
+	@Parameter T: Tableau
+	@Parameter columns: The vector with indexes of a base of LP
+	
+	Return value: 
+		Firs value of 5-tuple: True if the L.P has optimal solution or False 
+							   if the L.P is unlimited.
 		
-		Return value: 
-			- Pair (optimal_solution,x), where optimal_solution is the value 
-			  of the optimal solution for the given LP; x is the vector of 
-			  variables, with their values in the optimal solution.
-
-			- If the LP is unlimited, -1 will be returned.
+		optimal_solution: The value of the optimal solution.
+		x: Vector of variables, with their values in the optimal solution.
+		certificate: If the L.P has an optimal solution, it is the certificate 
+					 of optimality. If the L.P is unlimited, it is the certificate of unlimitedness.
+		columns: Vector of variables indexes that make up a feasible base in 
+				 the L.P. It is used in the auxiliar L.P.
 	"""
+
 	T_cert = np.vstack((np.zeros((T.shape)[0]-1),np.identity((T.shape)[0]-1))) 
 
 	# Transform all integers in fractions
 	T_cert = T_cert.astype("object")
 	old_shape = T_cert.shape
-	T_cert = np.array([frac.Fraction(int(T_cert[i][j]),1) for i in range((T_cert.shape)[0]) for j in range((T_cert.shape)[1])]).reshape(old_shape)
+	T_cert = np.array([frac.Fraction(T_cert[i][j]) for i in range((T_cert.shape)[0]) for j in range((T_cert.shape)[1])]).reshape(old_shape)
 
 	# Matrix used to find optimality certificate
 	T,T_cert = makeCanonical(T,T_cert,columns)
@@ -256,11 +266,12 @@ def simplex(T,columns):
 				if len(neg) == 0:
 					# Unlimited LP
 					certificate = np.array([]).astype("object")
-					certificate = np.array([frac.Fraction(0,1) for i in range((T.shape)[1]-1)])
-					# certificate = np.zeros((T.shape)[1]-1).astype("object")
+					certificate = np.array([frac.Fraction(0) for i in range((T.shape)[1]-1)])
+
 					for i in range(len(columns)):
 						certificate[columns[i]] = -T[i+1,col_index]
-					certificate[col_index] = frac.Fraction(1,1);
+					certificate[col_index] = frac.Fraction(1);
+					
 					return False,0,[],certificate,[]
 				else:
 					no_negative = True
@@ -269,10 +280,10 @@ def simplex(T,columns):
 					T,T_cert = makeCanonical(T,T_cert,columns)
 					break
 
-	x = np.array([])
+	x = np.array([]).astype("object")
 	for variable in range((T.shape)[1]-1):
 		if variable not in columns:
-			x = np.hstack((x,[0]))
+			x = np.hstack((x,frac.Fraction(0)))
 		else:
 			x = np.hstack((x,T[list(columns).index(variable)+1,-1]))
 	
@@ -282,6 +293,18 @@ def simplex(T,columns):
 	return True, optimal_solution, x, certificate, columns
 
 def findBasicSolution(A,b,c):
+	"""
+	@Parameter A: Matrix of constraints
+	@Parameter b: Vector of independent terms.
+	@Parameter c: Vector of coeficients.
+
+	Return:
+		First value of 3-tuple: True if the L.P is feasible or False otherwise.
+		columns: A vector with indexes of a base of LP
+		certificate: If the L.P is not feasible, it is a certificate of 
+					 infeasibility.
+	"""
+
 	# Remove negative values in b
 	for line in range(len(b)):
 		if b[line] < 0:
@@ -296,7 +319,7 @@ def findBasicSolution(A,b,c):
 
 	T = T.astype("object")
 	old_shape = T.shape
-	T = np.array([frac.Fraction(int(T[i][j]),1) for i in range((T.shape)[0]) for j in range((T.shape)[1])]).reshape(old_shape)
+	T = np.array([frac.Fraction(T[i][j]) for i in range((T.shape)[0]) for j in range((T.shape)[1])]).reshape(old_shape)
 
 	columns = [x for x in range(len(c),len(c)+len(b))]
 
@@ -306,9 +329,31 @@ def findBasicSolution(A,b,c):
 	return feasible_solution == 0, columns, certificate
 
 def printSolution(out_file,status,solution=None,x=None,certificate=[],position=None):
+
+	"""
+	@Parameter out_file: directory/name of the output file the L.P will be 
+						 written.
+	@Parameter status: String containing "otimo" if the L.P has an optimal 
+					   solution, "inviavel" if the L.P is unfeasible, 
+					   "ilimitado" if the L.P is unlimited.
+	@Parameter solution: Value of the optimal solution (if the L.P has an 
+						 optimal solution).
+	@Parameter x: Vector of varibles with their value in the optimal solution
+				  (if the L.P has an optimal solution).
+	@Parameter certificate: Certificate of optimality, infeasibility or 
+							unlimitedness.
+	@Parameter position: Dictionary generated in function transformFPI.
+	"""
+
 	output = "Status: " + str(status) + "\n"
 
+	for i in range(len(certificate)):
+		certificate[i] = certificate[i].limit_denominator()
+
 	if status == "otimo":
+		for i in range(len(x)):
+			x[i] = x[i].limit_denominator()
+		solution = solution.limit_denominator()
 		output += "Objetivo: " + str(solution) + "\n" + "Solucao:\n"
 		for _,value in position.items():
 			if type(value) == type((0,0)):
@@ -328,7 +373,7 @@ def printSolution(out_file,status,solution=None,x=None,certificate=[],position=N
 	f.close()
 
 def main():
-	np.set_printoptions(precision=2)
+	
 	if len(sys.argv) < 3:
 		print("Invalid number of arguments!")
 		sys.exit()
@@ -337,6 +382,7 @@ def main():
 	out_file = sys.argv[2]
 
 	A,b,c,signals,neg_constraints = readLP(inp_file)
+	n = len(A[0]) # Number of variables
 	number_variables = len(c)
 	A,c,position = transformFPI(A,c,signals,neg_constraints)
 
@@ -344,7 +390,6 @@ def main():
 
 	if not isFeasible:
 		printSolution(out_file,"inviavel",certificate=certificate)
-		# unfeasible(A,b,c,certificate)
 		return 0
 
 	# Build tableau
@@ -353,14 +398,15 @@ def main():
 	# Transform all integers in fractions
 	T = T.astype("object")
 	old_shape = T.shape
-	T = np.array([frac.Fraction(int(T[i][j]),1) for i in range((T.shape)[0]) for j in range((T.shape)[1])]).reshape(old_shape)
-
+	T = np.array([frac.Fraction(T[i][j]) for i in range((T.shape)[0]) for j in range((T.shape)[1])]).reshape(old_shape)
+	
 	# Run simplex
 	NotUnlimited, solution, x, certificate, _ = simplex(T,columns)
 
+	certificate = certificate[:n]
+
 	if not NotUnlimited:
 		printSolution(out_file,"ilimitado",[],[],certificate,position)
-		# unlimited(A,b,c,certificate)
 		return 0
 	
 	printSolution(out_file,"otimo",solution,x,certificate,position)
